b0531e2b9f31f1f7e19206e45f4de9e254cfcad8
volkan <volkankanat.mdbf17@iste.edu.tr>
2024-05-09 11:11:55 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('Hello World!')
+</script>
+
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
