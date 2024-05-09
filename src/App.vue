64c098b9606938576791c35210905b2ea3a33e31
volkan <volkankanat.mdbf17@iste.edu.tr>
2024-05-09 12:35:01 +0300

    feat: Add font Poppins for project styling

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,12 @@ const message = ref('Hello World!')
 </script>
 
 <template>
-  <div class="bg-slate-500 p-5 rounded">
+  <div class="font-poppins  bg-slate-300 p-5 rounded">
     <h1 class="text-3xl bg-yellow-300 text-red-800">{{ message }}</h1>
+    <p class="font-normal">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    <p class="font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    <p class="font-semibold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    <p class="font-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+      fontWeight: {
+        'light': '300',
+        'normal': '400',
+        'medium': '500',
+        'semibold': '600',
+        'bold': '700',
+      },
+    },
   },
   variants: {
     extend: {},
