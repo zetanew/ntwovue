94f3d6ef01a5f695555a72ec885ec1eda0daa72f
volkan <volkankanat.mdbf17@iste.edu.tr>
2024-05-10 07:38:58 +0300

    feat : List Posts

--- src/components/PostsPage.vue
+++ src/components/PostsPage.vue
@@ -1,36 +1,60 @@
 <template>
-    <div>
-      <a @click="goBack">Go Back</a>
-      <h1>Posts Page</h1>
-      <p>User ID: {{ userId }}</p>
+  <div>
+    <div @click="goBack" class="menu-item">
+      <button class="flex items-center hover:border-l-4 hover:border-purple-500 hover:bg-purple-100 rounded p-2 transition-all duration-200 w-full">
+        <img :src="BackIcon" alt="Go Back" />
+      </button>
     </div>
-  </template>
-  
-  <script>
-  import { ref, onMounted } from 'vue'
-  import { useRoute, useRouter } from 'vue-router'
-  import { useUserStore } from '@/store/userStore'
-  
-  export default {
-    setup() {
-      const route = useRoute()
-      const router = useRouter()
-      const userStore = useUserStore()
-      const userId = ref(null)
-  
-      onMounted(() => {
-        userId.value = route.params.id
-      })
-  
-      const goBack = () => {
-        userStore.selectUser(null)
-        router.push('/')
-      }
-  
-      return {
-        userId,
-        goBack
-      }
+    <div v-if="userDetails">
+      <div v-if="posts">
+        <h1 class="text-2xl font-bold mb-4">Posts</h1>
+        
+        <ul>
+          <li v-for="post in posts" :key="post.id" class="p-2 my-2 rounded shadow">
+            <h2 class="text-lg font-semibold text-purple-500">{{ post.title }}</h2>
+            <p class="text-gray-600">{{ post.body }}</p>
+          </li>
+        </ul>
+
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted, computed } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { useUserStore } from '@/store/userStore'
+import BackIcon from '@/assets/icons/back.png'
+export default {
+  setup() {
+    const route = useRoute()
+    const router = useRouter()
+    const userStore = useUserStore()
+    const userId = ref(null)
+
+    onMounted(() => {
+      userId.value = route.params.id
+      userStore.fetchUserDetails(userId.value)
+    })
+
+    const goBack = () => {
+      userStore.selectUser(null)
+      router.push('/')
+    }
+
+    const posts = computed(() => {
+      return userStore.userDetails ? userStore.userDetails.post : null
+    })
+
+    return {
+      userId,
+      goBack,
+      BackIcon,
+      userDetails: userStore.userDetails,
+      posts
     }
   }
-  </script>
+}
+</script>
